c0748ac2f6023a5009c5f89e2ceeaf3a9fc6f13e
llucas1 <email@llucassilva088@gmail.com>
2021-02-26 12:26:27 -0400

    subindo o projeto

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,7 @@
  // importar a dependencia do sqlite3
  const sqlite3 = require("sqlite3").verbose()
 
- //criar o objeto que ira faer operaçoes no banco de dados
+ //criar o objeto que ira fazer operaçoes no banco de dados
 
  const db = new sqlite3.Database("./src/database/database.db")
  
@@ -20,11 +20,44 @@
                 items text
             );
         `)
-
      // 2 inserir dados na tabela
+
+     const query = `
+        insert into places (
+            name,
+            image,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) values (?,?,?,?,?,?,?);
+    `
+
+    const values =  [
+        "Colectoria",
+        "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1101&q=80",
+        "Guilherme Gemballa, Jardim América",
+        "N° 260",
+        "Santa Catarina",
+        "Rio do Sul",
+        "Resíduos eletrônicos, lâmpadas"
+     ]
+     
+     function afterInsertData(err) {
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+     }
+     db.run (query, values, afterInsertData)
+
+     //db.run(query, valores, afterInsertData)
 
 
      // 3 consultar dados da tabela
+
 
 
      // 4 deletar dados na tabela
